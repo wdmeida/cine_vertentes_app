8bf5b0f8a62a9f2b222c9a94e43108c44e3a5550
Wagner Almeida <wdmeida@gmail.com>
2017-01-08 16:17:01 -0200

    Mudança nas cores do app.

---
+++ app/src/main/java/activities/cineplaza/ifsudestemg/br/cineplazaapp/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package activities.cineplaza.ifsudestemg.br.cineplazaapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import activities.cineplaza.ifsudestemg.br.cineplazaapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
